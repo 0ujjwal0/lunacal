edcdf24137119f6afe084c3f7b85a9d6013e2be1
ujjwal <ujjwal.12100544@lpu.in>
2024-09-04 21:41:21 +0530

    bottom widget

--- src/components/rightSection/bottomwidget/BottomWidgetNav.jsx
+++ src/components/rightSection/bottomwidget/BottomWidgetNav.jsx
@@ -5,12 +5,12 @@ const BottomWidgetNav = () => {
         <div className="w-1/3    lg:w-1/4  rounded-xl bg-black flex items-center justify-center">
           <button className="  text-gray-400  ">Gallery</button>
         </div>
-        <div className="w-1/2 bg-white">
-          <button className="h-full w-1/3  lg:w-1/3  text-gray-400 hover:text-white hover:bg-gray-700 rounded-xl hover:shadow-2xl hover:shadow-black">
-            Add Image
+        <div className="w-1/2 flex justify-end gap-4">
+          <button className="text-xs h-full w-1/3 lg:w-1/3 bg-[#363c43] hover:text-white hover:bg-gray-700 rounded-3xl shadow-[inset_1px_1px_3px_rgba(255,255,255,0.3),8px_8px_7px_rgba(0,0,0,0.4),_-3px_-3px_3px_rgba(255,255,255,0.3)]">
+           + ADD IMAGE
           </button>
-          <button className="h-full w-1/3  lg:w-1/6 text-gray-400 hover:text-white hover:bg-gray-700 rounded-full hover:shadow-2xl hover:shadow-black">
-            +
+          <button className=" w-auto  lg:w-1/6 text-gray-400 rounded-full hover:shadow-2xl hover:shadow-black">
+            <img className="hover:shadow-xl rounded-full " src="/left1.png" alt="" />
           </button>
           <button className="h-full w-1/3  lg:w-1/6 text-gray-400 hover:text-white hover:bg-gray-700 rounded-full hover:shadow-2xl hover:shadow-black">
             -
